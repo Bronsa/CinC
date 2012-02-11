e6d4ea03513cd5a8211ea44dbfce5cb83d247e0b
Joel "Aaron" Cohen <aaron@assonance.org>
2012-02-11 14:57:42 -0500

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject cinc "0.0.1-SNAPSHOT"
+  :description "Clojure in Clojure."
+  :url "https://bitbucket.org/remleduff/cinc"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.ow2.asm/asm "4.0"]
+                 [org.ow2.asm/asm-commons "4.0"]
+                 [org.ow2.asm/asm-util "4.0"]
+                 [org.ow2.asm/asm-analysis "4.0"]])
