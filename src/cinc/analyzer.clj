58a4b8a30059907c453e5f908bf40b1a2b4c0f39
Bronsa <brobronsa@gmail.com>
2013-06-26 23:49:44 +0200

    fix 'if parsing

--- src/cinc/analyzer.clj
+++ src/cinc/analyzer.clj
@@ -273,7 +273,7 @@
         (analyze-block exprs env)))
 
 (defmethod parse 'if
-  [[_ test then [& else] :as form] env]
+  [[_ test then & else :as form] env]
   {:pre [(or (= 3 (count form))
              (= 4 (count form)))]}
   (let [test (analyze test (ctx env :expr))
