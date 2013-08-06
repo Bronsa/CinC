e75b3072799da8b60f423a63325c4061c8dff994
Bronsa <brobronsa@gmail.com>
2013-08-06 22:21:17 +0200

    add emit for :set, :map, :vector

--- src/cinc/compiler/jvm/bytecode/emit.clj
+++ src/cinc/compiler/jvm/bytecode/emit.clj
@@ -1,15 +1,46 @@
 (ns cinc.compiler.jvm.bytecode.emit)
 
-(defmulti emit :op)
+(defmulti -emit :op)
 
-(defmethod emit :import
-  [{:keys [class env]}]
-  (into
-   [[:get-static :rt/current-ns]
-    [:invoke-virtual [:deref]]
-    [:check-cast :ns]
-    [:push class]
-    [:invoke-static [:class/for-name :string]]
-    [:invoke-virtual [:import-class :class]]]
-   (if (= :statement (:context env))
-     [[:pop]])))
+(defn emit
+  [{:keys [env] :as ast}]
+  (into (-emit ast)
+        (if (= :statement (:context env))
+          [[:pop]])))
+
+(defmethod -emit :import
+  [{:keys [class]}]
+  [[:get-static :rt/current-ns]
+   [:invoke-virtual [:deref]]
+   [:check-cast :ns]
+   [:push class]
+   [:invoke-static [:class/for-name :string]]
+   [:invoke-virtual [:import-class :class]]])
+
+(defn emit-as-array [list]
+  (into [[:push (int (count list))]
+         [:new-array :object]]
+        (mapcat (fn [i item]
+                  [[:dup]
+                   [:push (int i)]
+                   (emit item)
+                   [:array-store :object]])
+                (range) list)))
+
+(defmethod -emit :map
+  [{:keys [keys vals]}]
+  (conj
+   (emit-as-array (interleave keys vals))
+   [:invoke-static [:rt/map-unique-keys :objects]]))
+
+(defmethod -emit :vector
+  [{:keys [items]}]
+  (conj
+   (emit-as-array items)
+   [:invoke-static [:rt/vector :objects]]))
+
+(defmethod -emit :set
+  [{:keys [items]}]
+  (conj
+   (emit-as-array items)
+   [:invoke-static [:rt/set :objects]]))
