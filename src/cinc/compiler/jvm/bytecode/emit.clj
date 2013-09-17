e48ed9b318cc1963d777756b0b560a344dcf3394
Bronsa <brobronsa@gmail.com>
2013-09-17 19:16:03 +0200

    fix nil handling

--- src/cinc/compiler/jvm/bytecode/emit.clj
+++ src/cinc/compiler/jvm/bytecode/emit.clj
@@ -21,10 +21,12 @@
      [[:invoke-static [:clojure.lang.RT/box tag] :java.lang.Character]]
      (= Boolean box)
      [[:invoke-static [:clojure.lang.RT/box tag] :java.lang.Boolean]])
-    [[:invoke-static [(keyword "clojure.lang.RT"
-                               (str (.getName ^Class box)
-                                    "Cast")) (if (primitive? tag) tag
-                                                 :java.lang.Object)] box]]))
+    (if (nil? tag)
+      [[:insn :ACONST_NULL]]
+      [[:invoke-static [(keyword "clojure.lang.RT"
+                                 (str (.getName ^Class box)
+                                      "Cast")) (if (primitive? tag) tag
+                                                   :java.lang.Object)] box]])))
 
 (defn emit-cast [tag cast]
   (if (not (or (primitive? tag)
