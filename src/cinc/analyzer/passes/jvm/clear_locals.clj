8bdeaccb5d8f4def384534fb9e460f589fa1c7ef
Bronsa <brobronsa@gmail.com>
2013-09-21 00:21:41 +0200

    style fix

--- src/cinc/analyzer/passes/jvm/clear_locals.clj
+++ src/cinc/analyzer/passes/jvm/clear_locals.clj
@@ -1,22 +1,21 @@
 (ns cinc.analyzer.passes.jvm.clear-locals
   (:require [cinc.analyzer.passes :refer [walk]]))
 
-
-(def ^:dynamic *clears* {:branch-clears #{}
-                         :clears        #{}
-                         :closes        #{}})
+(def clears (atom  {:branch-clears #{}
+                    :clears        #{}
+                    :closes        #{}}))
 
 (defn -clear-locals
   [{:keys [op name local path? should-not-clear env] :as ast}]
   (if (and (= :local op)
            (#{:let :loop :letfn :arg} local)
-           (or (not ((:closes *clears*) name))
+           (or (not ((:closes @clears) name))
                (:once env))
-           (not ((:clears *clears*) name))
+           (not ((:clears @clears) name))
            (not should-not-clear))
     (do
-      (update! *clears* update-in [:branch-clears] conj name)
-      (update! *clears* update-in [:clears] conj name)
+      (swap! clears update-in [:branch-clears] conj name)
+      (swap! clears update-in [:clears] conj name)
       (assoc ast :to-clear? true))
     ast))
 
@@ -24,20 +23,19 @@
   [{:keys [path? branch?] :as ast}]
   (let [ast (-clear-locals ast)]
     (if path?
-      (doseq [c (:clears *clears*)]
-        (when ((:branch-clears *clears*) c)
-          (update! *clears* update-in [:clears] disj c)))
+      (doseq [c (:clears @clears)]
+        (when ((:branch-clears @clears) c)
+          (swap! clears update-in [:clears] disj c)))
       (when branch?
-        (update! *clears* update-in [:clears] into (:branch-clears *clears*))
-        (update! *clears* assoc :branch-clears #{})))
+        (swap! clears update-in [:clears] into (:branch-clears @clears))
+        (swap! clears assoc :branch-clears #{})))
     ast))
 
 (defn -propagate-closed-overs
   [{:keys [op closed-overs] :as ast}]
   (when (#{:reify :fn :deftype} op)
-    (update! *clears* assoc-in [:closes] (or closed-overs #{})))
+    (swap! clears assoc-in [:closes] (or closed-overs #{})))
   ast)
 
 (defn clear-locals [ast]
-  (binding [*clears* *clears*]
-    (walk ast -propagate-closed-overs clear-locals-around :reversed)))
+  (walk ast -propagate-closed-overs clear-locals-around :reversed))
